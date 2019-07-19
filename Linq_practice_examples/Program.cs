84ceb49530ac147fae6d3cc496dd25a443a4fe55
Aki- <akshaydalvi24@gmail.com>
2019-07-19 10:18:24 +0530

    Practice routine 2 added. 1. Use of "let" keyword 2. Another way to use "select" keyword.

--- Linq_practice_examples/Program.cs
+++ Linq_practice_examples/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Linq_practice_examples
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            //printEvenNums();
+            getNumberWithSquare();
+
+
+
+
+
+            Console.Read();
+
+        }
+
+
+
+        public static void printEvenNums()
+        {
             //Practice routine 1
-            
+
             //Problem: WAP to print even number from int array of 10
             //Write in Both styles: 1. QUERY Expression   2. Method Syntax.
 
@@ -16,27 +36,58 @@ namespace Linq_practice_examples
             int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 
-            //var evenNum = from x in nums
-            //              where (x % 2) == 0
-            //              select x;
+            var evenNum = from x in nums
+                          where (x % 2) == 0
+                          select x;
 
-            //foreach(int i in evenNum)
-            //Console.WriteLine(i);
+            foreach (int i in evenNum)
+                Console.WriteLine(i);
+
+            IEnumerable<int> sumNums = nums.Where(x => (x % 2) == 0);
+            foreach (int i in sumNums)
+                Console.WriteLine(i);
+        }
+
+        public static void getNumberWithSquare()
+        {
+            //Find the number and its square of an array which is more than 20
+
+            //Expected Output : 
+            //{ Number = 9, SqrNo = 81 } 
+            //{ Number = 8, SqrNo = 64 }            
+
+            //{ Number = 3, SqrNo = NA } SHOULD NOT PRINT ANYTHING
 
 
 
+            int[] arr1 = new[] { 3, 9, 2, 8, 6, 5 };
 
-            //IEnumerable<int> sumNums = nums.Where(x => (x % 2) == 0);
-            //foreach (int i in sumNums)
-            //    Console.WriteLine(i);
 
+            var numberWithSquare = from int n in arr1
+                                   let sqNum = n * n
+                                   where sqNum > 20
+                                   select new { n, sqNum };
+
+
+            foreach(var v in numberWithSquare)
+                Console.WriteLine(v);
 
 
             Console.Read();
 
 
 
-
         }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
